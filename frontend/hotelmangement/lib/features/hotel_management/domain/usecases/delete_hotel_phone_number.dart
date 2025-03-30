3568563d99d2330225bc2f6f084e00b8841b69f1
galahirudilhara <galahirudilhara@gmail.com>
2025-03-30 23:09:44 +0530

    change the deletehotelphonenumber to more efficient validation. also updated the test

--- frontend/hotelmangement/lib/features/hotel_management/domain/usecases/delete_hotel_phone_number.dart
+++ frontend/hotelmangement/lib/features/hotel_management/domain/usecases/delete_hotel_phone_number.dart
@@ -10,16 +10,76 @@ class DeleteHotelPhoneNumber extends Usecase<int, Params> {
   DeleteHotelPhoneNumber({required this.repository});
 
   @override
-  Future<Either<Failure, int>> call(Params params) {
-    return repository.deleteHotelPhoneNumber(params.phoneNumberId);
+  Future<Either<Failure, int>> call(Params params) async {
+    final validateResult = await DeleteHotelPhoneNumberValidate(
+        params: params, repository: repository)();
+
+    return validateResult.fold(
+      (failure) => Left(failure),
+      (_) {
+        return repository.deleteHotelPhoneNumber(params.phoneNumberId);
+      },
+    );
   }
 }
 
 class Params extends Equatable {
   final String phoneNumberId;
+  final String hotelId;
+  final String managerId;
 
-  const Params({required this.phoneNumberId});
+  const Params(
+      {required this.phoneNumberId,
+      required this.hotelId,
+      required this.managerId});
 
   @override
   List<Object?> get props => [phoneNumberId];
+}
+
+class DeleteHotelPhoneNumberValidate {
+  final HotelRepository repository;
+  final Params params;
+
+  DeleteHotelPhoneNumberValidate(
+      {required this.repository, required this.params});
+
+  Future<Either<Failure, int>> call() async {
+    final validators = [_validateManagerId, _validatePhoneNumberExistance];
+    for (final validator in validators) {
+      final result = await validator();
+      if (result.isLeft()) {
+        return result;
+      }
+    }
+    return Right(1);
+  }
+
+  Future<Either<Failure, int>> _validateManagerId() async {
+    final hotelOrFailure = await repository.getHotel(params.hotelId);
+    return hotelOrFailure.fold(
+      (failure) => Left(failure),
+      (hotel) {
+        if (hotel.managerId != params.managerId) {
+          return Left(UnAuthorizedFailure());
+        }
+        return Right(1);
+      },
+    );
+  }
+
+  Future<Either<Failure, int>> _validatePhoneNumberExistance() async {
+    final hotelPhoneNumbersOrFailure =
+        await repository.getHotelPhoneNumbers(params.hotelId);
+    return hotelPhoneNumbersOrFailure.fold(
+      (failure) => Left(failure),
+      (phoneNumbers) {
+        if (phoneNumbers
+            .any((phoneNumber) => phoneNumber.id == params.phoneNumberId)) {
+          return Right(1);
+        }
+        return Left(NotFound());
+      },
+    );
+  }
 }
--- frontend/hotelmangement/test/features/hotel_management/domain/usecases/delete_hotel_phone_number_test.dart
+++ frontend/hotelmangement/test/features/hotel_management/domain/usecases/delete_hotel_phone_number_test.dart
@@ -1,5 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hotelmangement/core/error/failure.dart';
+import 'package:hotelmangement/features/hotel_management/domain/entities/hote_phone_number.dart';
+import 'package:hotelmangement/features/hotel_management/domain/entities/hotel.dart';
 import 'package:hotelmangement/features/hotel_management/domain/repositories/hotel_repository.dart';
 import 'package:hotelmangement/features/hotel_management/domain/usecases/delete_hotel_phone_number.dart';
 import 'package:mockito/annotations.dart';
@@ -22,15 +25,114 @@ void main() {
     () async {
       // arrange
       final phoneNumberId = "1";
+      final hotelId = "1";
+      final managerId = "1";
+      final hotel = Hotel(
+          id: "1",
+          managerId: "1",
+          name: "testName",
+          address: "testAddress",
+          latitude: 10.0,
+          longitude: 20.0);
+      final hotelPhoneNumbers = [
+        HotelPhoneNumber(
+            id: "1", role: "testRole", phoneNumber: "0", hotelId: hotelId),
+        HotelPhoneNumber(
+            id: "2", role: "testRole", phoneNumber: "1", hotelId: hotelId),
+        HotelPhoneNumber(
+            id: "3", role: "testRole", phoneNumber: "2", hotelId: hotelId),
+        HotelPhoneNumber(
+            id: "4", role: "testRole", phoneNumber: "4", hotelId: hotelId),
+      ];
       when(repository.deleteHotelPhoneNumber(phoneNumberId))
           .thenAnswer((_) async => Right(1));
+      when(repository.getHotel(hotelId)).thenAnswer((_) async => Right(hotel));
+      when(repository.getHotelPhoneNumbers(hotelId))
+          .thenAnswer((_) async => Right(hotelPhoneNumbers));
 
       // act
-      final result = await usecase(Params(phoneNumberId: phoneNumberId));
+      final result = await usecase(Params(
+        phoneNumberId: phoneNumberId,
+        hotelId: hotelId,
+        managerId: managerId,
+      ));
 
       // assert
       expect(result, Right(1));
+      verify(repository.getHotel(hotelId)).called(1);
+      verify(repository.getHotelPhoneNumbers(hotelId)).called(1);
       verify(repository.deleteHotelPhoneNumber(phoneNumberId)).called(1);
+      verifyNoMoreInteractions(repository);
+    },
+  );
+
+  test(
+    'should return UnAuthorized Failure when the managerId are not maching',
+    () async {
+      // arrange
+      final phoneNumberId = "1";
+      final hotelId = "1";
+      final managerId = "1";
+      final hotel = Hotel(
+          id: "1",
+          managerId: "2",
+          name: "testName",
+          address: "testAddress",
+          latitude: 10.0,
+          longitude: 20.0);
+      when(repository.getHotel(hotelId)).thenAnswer((_) async => Right(hotel));
+
+      // act
+      final result = await usecase(Params(
+        phoneNumberId: phoneNumberId,
+        hotelId: hotelId,
+        managerId: managerId,
+      ));
+
+      // assert
+      expect(result, Left(UnAuthorizedFailure()));
+      verify(repository.getHotel(hotelId)).called(1);
+      verifyNoMoreInteractions(repository);
+    },
+  );
+
+  test(
+    'should return NotFound Failure when the phoneNumberId not exists',
+    () async {
+      // arrange
+      final phoneNumberId = "1";
+      final hotelId = "1";
+      final managerId = "1";
+      final hotel = Hotel(
+          id: "1",
+          managerId: "1",
+          name: "testName",
+          address: "testAddress",
+          latitude: 10.0,
+          longitude: 20.0);
+      final hotelPhoneNumbers = [
+        HotelPhoneNumber(
+            id: "2", role: "testRole", phoneNumber: "1", hotelId: hotelId),
+        HotelPhoneNumber(
+            id: "3", role: "testRole", phoneNumber: "2", hotelId: hotelId),
+        HotelPhoneNumber(
+            id: "4", role: "testRole", phoneNumber: "4", hotelId: hotelId),
+      ];
+      when(repository.getHotel(hotelId)).thenAnswer((_) async => Right(hotel));
+      when(repository.getHotelPhoneNumbers(hotelId))
+          .thenAnswer((_) async => Right(hotelPhoneNumbers));
+
+      // act
+      final result = await usecase(Params(
+        phoneNumberId: phoneNumberId,
+        hotelId: hotelId,
+        managerId: managerId,
+      ));
+
+      // assert
+      expect(result, Left(NotFound()));
+      verify(repository.getHotel(hotelId)).called(1);
+      verify(repository.getHotelPhoneNumbers(hotelId)).called(1);
       verifyNoMoreInteractions(repository);
     },
   );
