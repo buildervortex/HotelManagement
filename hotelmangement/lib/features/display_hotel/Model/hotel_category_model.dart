fd134f742a9bd12ab8863983810d4c51688766fc
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-05-09 23:51:49 +0530

    Create hotel category model in display hotel

---
+++ hotelmangement/lib/features/display_hotel/Model/hotel_category_model.dart
@@ -0,0 +1,19 @@
+class HotelCategory {
+  final String name;
+
+  HotelCategory({required this.name});
+}
+
+List<HotelCategory> hotelcategory = [
+  HotelCategory(name: "All"),
+  HotelCategory(name: "Resorts"),
+  HotelCategory(name: "Villas"),
+  HotelCategory(name: "Luxury Hotels"),
+  HotelCategory(name: "Boutique Hotels"),
+  HotelCategory(name: "Eco Hotels"),
+  HotelCategory(name: "Heritage Hotels"),
+];
+
+
+
+ 
