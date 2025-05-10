b06b82ee99472c8c6eec88544cf83efd069b6785
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-05-10 23:14:24 +0530

    Create hotel details in card Widgect

--- hotelmangement/lib/features/display_hotel/Views/Widgects/hotel_card.dart
+++ hotelmangement/lib/features/display_hotel/Views/Widgects/hotel_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hotel_management/Model/hotel_model.dart';
 
 class HotelCard extends StatelessWidget {
   final HotelModel hotelItems;
@@ -24,8 +25,101 @@ class HotelCard extends StatelessWidget {
           height: size.height * 0.20,
           width: size.width * 0.9,
         ),
+        const SizedBox(height: 7),
 
-      
+        // Hotel Details
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Hotel Name
+                  Text(
+                    hotelItems.name.toString(),
+                    style: const TextStyle(
+                      color: Colors.black87,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+
+                  // Location
+                  Row(
+                    children: [
+                      const Icon(Icons.location_on,
+                          color: Colors.blueGrey, size: 18),
+                      Text(
+                        hotelItems.location.toString(),
+                        style: const TextStyle(
+                            color: Colors.black54, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+
+                  // Beds & Bathrooms
+                  Row(
+                    children: [
+                      const Icon(Icons.bed, color: Colors.blueGrey, size: 18),
+                      Text(
+                        " ${hotelItems.bed} Bed",
+                        style: const TextStyle(
+                            color: Colors.black54, fontSize: 16),
+                      ),
+                      const SizedBox(width: 20),
+                      const Icon(Icons.bathtub,
+                          color: Colors.blueGrey, size: 18),
+                      Text(
+                        " ${hotelItems.bathroom} Bathroom",
+                        style: const TextStyle(
+                            color: Colors.black54, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.star, color: Color.fromARGB(255, 236, 213, 7), size: 18),
+                      SizedBox(width: 5),
+                      Text(
+                        hotelItems.rating.toString(),
+                        style:
+                            const TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  // Price
+                  Text(
+                    "Rs ${hotelItems.price}",
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  // Package
+                  Text(
+                    hotelItems.package,
+                    style: const TextStyle(
+                      color: Colors.black54,
+                      fontSize: 16,
+                      // fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
