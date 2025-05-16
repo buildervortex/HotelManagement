0913a302c895cf4c5e483a798125d326c44a753e
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-05-16 21:07:18 +0530

    Create Google Map

---
+++ hotelmangement/lib/features/google_map/google_map.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapFlutter extends StatefulWidget {
+  const GoogleMapFlutter({super.key});
+
+  @override
+  State<GoogleMapFlutter> createState() => _GoogleMapFlutterState();
+}
+
+class _GoogleMapFlutterState extends State<GoogleMapFlutter> {
+  LatLng myCurrentLocation = LatLng(80.8731, 80.7718);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition:
+            CameraPosition(target: myCurrentLocation, zoom: 15),
+        markers: {
+          Marker(
+            markerId: MarkerId("Marker Id"),
+            position: myCurrentLocation,
+            draggable: true,
+            onDragEnd: (value) {},
+            infoWindow: InfoWindow(
+              title: "Title of the maker",
+              snippet: "more info about marker",
+            ),
+          ),
+        },
+      ),
+    );
+  }
+}
