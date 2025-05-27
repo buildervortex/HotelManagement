de39f86d1ea0a794dbfedd2b0b003b96fd486bd4
galahirudilhara <galahirudilhara@gmail.com>
2025-05-27 14:21:03 +0530

    add the file url authenticator repository

--- hotelmangement/lib/features/hotel_management/data/repositories/file_repository_impl.dart
+++ hotelmangement/lib/features/hotel_management/data/repositories/file_repository_impl.dart
@@ -1,13 +1,24 @@
 import 'package:dartz/dartz.dart';
 import 'package:hotelmangement/core/error/failure.dart';
 import 'package:hotelmangement/features/hotel_management/domain/repositories/file_repository.dart';
-import 'package:supabase/supabase.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class FileRepositoryImpl implements FileRepository {
   final SupabaseClient supabaseClient;
+  final expiryDurationSeconds = 60 * 60 * 1; // 1 hour
+
+  FileRepositoryImpl({required this.supabaseClient});
+
   @override
   Future<Either<Failure, String>> getFileAuthUrl(
-      String filePath, String bucketName) {
-    throw UnimplementedError();
+      String filePath, String bucketName) async {
+    try {
+      final response = await supabaseClient.storage
+          .from(bucketName)
+          .createSignedUrl(filePath, expiryDurationSeconds);
+      return Right(response);
+    } on StorageException catch (e) {
+      return Left(ServerFailure());
+    }
   }
 }
