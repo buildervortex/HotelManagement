b1f743ffdb1fbc2499920fe22c17f4f27c8f86da
galahirudilhara <galahirudilhara@gmail.com>
2025-05-27 15:21:29 +0530

    starts and hold the repository implementation

---
+++ hotelmangement/lib/features/hotel_management/data/repositories/hotel_repository_impl.dart
@@ -0,0 +1,89 @@
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:hotelmangement/core/error/failure.dart';
+import 'package:hotelmangement/features/hotel_management/domain/entities/hotel.dart';
+import 'package:hotelmangement/features/hotel_management/domain/entities/hotel_image.dart';
+import 'package:hotelmangement/features/hotel_management/domain/entities/hotel_phone_number.dart';
+import 'package:hotelmangement/features/hotel_management/domain/repositories/hotel_repository.dart';
+import 'package:mime/mime.dart';
+import 'package:path/path.dart';
+
+class HotelRepositoryImpl implements HotelRepository {
+  @override
+  Future<Either<Failure, HotelImage>> addHotelImage(
+      String hotelId, String localImagePath, String remoteImageSaveName) {
+    // create file object
+    File file = File(localImagePath);
+
+    // get file base name
+    String fileBasName = basename(localImagePath);
+
+    // get content MIME type
+    String contentType = lookupMimeType(fileBasName) ?? 'image/jpeg';
+  }
+
+  @override
+  Future<Either<Failure, HotelPhoneNumber>> addHotelPhoneNumber(
+      String hotelId, String phoneNumber, String role) {
+    // TODO: implement addHotelPhoneNumber
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Hotel>> createHotel(String name, String address,
+      double longitude, double latitude, String managerId) {
+    // TODO: implement createHotel
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, void>> deleteHotel(String hotelId) {
+    // TODO: implement deleteHotel
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, int>> deleteHotelImage(String imageId) {
+    // TODO: implement deleteHotelImage
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, int>> deleteHotelPhoneNumber(String phoneNumberId) {
+    // TODO: implement deleteHotelPhoneNumber
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Hotel>> getHotel(String hotelId) {
+    // TODO: implement getHotel
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<HotelImage>>> getHotelImages(String hotelId) {
+    // TODO: implement getHotelImages
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<HotelPhoneNumber>>> getHotelPhoneNumbers(
+      String hotelId) {
+    // TODO: implement getHotelPhoneNumbers
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> isImageExists(String imageId, String hotelId) {
+    // TODO: implement isImageExists
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Hotel>> updateHotel(String hotelId, String? name,
+      String? address, double? longitude, double? latitude, String? mainImage) {
+    // TODO: implement updateHotel
+    throw UnimplementedError();
+  }
+}
