66fe6dbf1b62cf2632613a9c0141357d721a246e
galahirudilhara <galahirudilhara@gmail.com>
2025-05-28 00:42:05 +0530

    move test to seperate test

--- hotelmangement/lib/main.dart
+++ hotelmangement/lib/main.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hotelmangement/core/initialize.dart';
 import 'package:hotelmangement/features/homePage_rating_reviews/give_ratings_main.dart';
-import 'package:hotelmangement/features/homePage_rating_reviews/home_page_main.dart';
-import 'package:hotelmangement/features/homePage_rating_reviews/view_ratings_main.dart';
-import 'package:hotelmangement/features/booking/room_booking.dart';
-import 'package:hotelmangement/features/booking/table_booking.dart';
-import 'package:hotelmangement/features/google_map/google_map.dart';
-import 'package:hotelmangement/features/google_map/google_map_polygon.dart';
-// import 'package:hotelmangement/features/live_chat/screens/chat_list_screen.dart';
-import 'package:hotelmangement/features/display_hotel/display_hotel.dart';
-// import 'package:hotelmangement/features/google_map/google_map_polyline.dart';
-import 'package:hotelmangement/features/auth/login.dart';
-import "package:hotelmangement/features/booking_history/booking_history.dart";
-
-import 'package:hotelmangement/features/hotel_management/data/dataSources/file_data_source.dart';
-import 'package:hotelmangement/features/hotel_management/data/dataSources/hotel_management_data_source.dart';
-import 'package:hotelmangement/features/hotel_management/data/repositories/file_repository_impl.dart';
-import 'package:hotelmangement/features/hotel_management/data/repositories/hotel_repository_impl.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:hotelmangement/test.dart';
 
 void main() async {
   // ensure the flutter is initialized
@@ -27,7 +11,7 @@ void main() async {
   await initializeProject();
 
   // testing porpose only
-  // await testCall();
+  await initTest();
 
   runApp(const MyApp());
 }
@@ -95,22 +79,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
-
-Future<void> testCall() async {
-  final hotelId = "550e8400-e29b-41d4-a716-446655440001";
-  final managerId = "e8b2c4e6-353a-450d-ab3a-08a0676fd773";
-
-  final fileRepo = FileDataSourceImpl(client: Supabase.instance.client);
-  final hotelDataSource =
-      HotelManagementDataSourceImpl(client: Supabase.instance.client);
-
-  final repo = HotelRepositoryImpl(
-      dataSource: hotelDataSource, fileDataSource: fileRepo);
-
-  // await repo.addHotelPhoneNumber(hotelId, "0714587248", "admin-role");
-
-  await repo.createHotel("hotel1", "hoteladdress", 25.5, 12.5, managerId);
-  final hotelId1 = "06c25869-2af2-46e4-916b-800e6f00e419";
-  await repo.deleteHotel(hotelId1);
 }
