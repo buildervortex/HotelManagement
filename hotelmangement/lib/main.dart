829be4815e3270dda0481805a3c032866d064fae
hiruni <hmkooragodage7@gmail.com>
2025-05-27 19:14:33 +0530

    delete livechat

--- hotelmangement/lib/main.dart
+++ hotelmangement/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hotelmangement/core/initialize.dart';
-import 'package:hotelmangement/features/live_chat/screens/chat_list_screen.dart';
+import 'package:hotelmangement/features/auth/login.dart';
 
 
 void main() async {
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ChatListScreen(),
+      home: LoginScreen(),
     );
   }
 }
