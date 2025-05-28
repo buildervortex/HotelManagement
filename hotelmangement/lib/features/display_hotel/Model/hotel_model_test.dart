04b8356ec6df228845e7928035c2991a1a398631
galahirudilhara <galahirudilhara@gmail.com>
2025-05-28 22:20:35 +0530

    fix issues

--- hotelmangement/lib/features/display_hotel/Model/hotel_model_test.dart
+++ hotelmangement/lib/features/display_hotel/Model/hotel_model_test.dart
@@ -11,7 +11,7 @@ class DisplayHotelDetails extends StatefulWidget {
 class _DisplayHotelDetailsState extends State<DisplayHotelDetails> {
   final supabase = Supabase.instance.client;
 
-  List<Hotel> hotels = [];
+  List<ExtendedHotel> extendedHotels = [];
 
   @override
   void initState() {
@@ -21,28 +21,70 @@ class _DisplayHotelDetailsState extends State<DisplayHotelDetails> {
 
   void _loadHotels() async {
     final hotelList = await supabase.from("hotel").select();
+    List<Hotel> hotels = [];
+    List<ExtendedHotel> extHotel = [];
+
+    hotels = hotelList.map((hotel) => Hotel.fromJson(hotel)).toList();
+    for (Hotel hotel in hotels) {
+      final images =
+          await supabase.from("hotel_image").select().eq("hotel_id", hotel.id);
+
+      final phoneNumbers = await supabase
+          .from("hotel_phone_number")
+          .select()
+          .eq("hotel_id", hotel.id);
+      List<String> hotelImageUrlList = [];
+
+      for (var image in images) {
+        final url = await supabase.storage
+            .from("hotelimage")
+            .createSignedUrl(image["file"], 60 * 60 * 60);
+        hotelImageUrlList.add(url);
+      }
+
+      extHotel.add(ExtendedHotel(
+        hotel: hotel,
+        phoneNumbers:
+            phoneNumbers.map((num) => HotelPhoneNumber.fromJson(num)).toList(),
+        hotelImageList: hotelImageUrlList,
+      ));
+    }
+
     setState(() {
-      hotels = hotelList.map((hotel) => Hotel.fromJson(hotel)).toList();
+      this.extendedHotels = extHotel;
     });
     print(hotels);
     print(hotelList);
   }
-
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Hotel Room Prices")),
       body: ListView.builder(
-        itemCount: hotels.length,
+        itemCount: extendedHotels.length,
         itemBuilder: (context, index) {
-          final hotel = hotels[index];
+          final hotel = extendedHotels[index];
           return Card(
             margin: const EdgeInsets.all(10),
             child: ListTile(
-              title: Text(hotel.name),
-              subtitle: Text("Address: ${hotel.address}"),
+              title: Column(
+                children: [
+                  Text(hotel.hotelImageList.first ?? "No Image Available"),
+                  Image.network(
+                      extendedHotels[index].hotelImageList.first ?? ""),
+                  Text(
+                    hotel.hotel.name,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    hotel.hotel.address,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+              subtitle: Text("Address: ${hotel.phoneNumbers.first.number}"),
             ),
           );
         },
@@ -84,4 +126,59 @@ class Hotel {
       createdAt: DateTime.parse(json['created_at'] as String),
     );
   }
+}
+
+class HotelImage {
+  final String id;
+  final String hotelId;
+  final String file;
+
+  const HotelImage({
+    required this.id,
+    required this.hotelId,
+    required this.file,
+  });
+
+  factory HotelImage.fromJson(Map<String, dynamic> json) {
+    return HotelImage(
+      id: json['id'] as String,
+      hotelId: json['hotel_id'] as String,
+      file: json['file'] as String,
+    );
+  }
+}
+
+class HotelPhoneNumber {
+  final String id;
+  final String hotelId;
+  final String number;
+  final String role;
+
+  const HotelPhoneNumber({
+    required this.id,
+    required this.hotelId,
+    required this.number,
+    required this.role,
+  });
+
+  factory HotelPhoneNumber.fromJson(Map<String, dynamic> json) {
+    return HotelPhoneNumber(
+      id: json['id'] as String,
+      hotelId: json['hotel_id'] as String,
+      number: json['number'] as String,
+      role: json['role'] as String,
+    );
+  }
+}
+
+class ExtendedHotel {
+  final Hotel hotel;
+  final List<HotelPhoneNumber> phoneNumbers;
+  final List<String> hotelImageList;
+
+  const ExtendedHotel({
+    required this.hotel,
+    required this.phoneNumbers,
+    required this.hotelImageList,
+  });
 }
