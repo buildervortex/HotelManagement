2cbf458ec7925d23b5e3d2a43fde55e2fbb46310
ravindu senanayake <ravindusenanayake10@gmail.com>
2025-05-28 17:28:16 +0530

    abcde

---
+++ hotelmangement/lib/features/booking/room_booking_details.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class BookingDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Room Booking Details"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Text("Room Booking Details",
+            //     style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            Text("Floor: 2nd", style: TextStyle(fontSize: 18)),
+            SizedBox(height: 10),
+            Text("Room Number: 202", style: TextStyle(fontSize: 18)),
+            SizedBox(height: 10),
+            Text("Guests: 3", style: TextStyle(fontSize: 18)),
+            SizedBox(height: 10),
+            Text("Check-in: June 10, 2025", style: TextStyle(fontSize: 18)),
+            SizedBox(height: 10),
+            Text("Check-out: June 15, 2025", style: TextStyle(fontSize: 18)),
+            SizedBox(height: 30),
+            Spacer(),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Room booked successfully!")),
+                  );
+                },
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  child: Text("Book Now", style: TextStyle(fontSize: 18)),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
