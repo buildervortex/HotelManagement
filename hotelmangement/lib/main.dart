c1a073c15a8cfc353a4cdf2d54316f00374ae341
DGNuwan <dgnuwanthilakarathna@gmail.com>
2025-05-30 03:44:05 +0530

    fix issue

--- hotelmangement/lib/main.dart
+++ hotelmangement/lib/main.dart
@@ -66,61 +66,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-<<<<<<< HEAD
-      home:RoomBookingPage(),
-
-      // home: GoogleMapPolyline(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-=======
       home: ManagerMainPage(
         managerId: "e8b2c4e6-353a-450d-ab3a-08a0676fd773",
->>>>>>> bce3ce0576e24d37471919bea68f1cda34341cd7
       ),
     );
   }
