b2b1e195ff4fb48243e65bd361279a2d9526975d
galahirudilhara <galahirudilhara@gmail.com>
2025-05-30 11:55:33 +0530

    creating the google map page

---
+++ hotelmangement/lib/features/hotel_management/presentation/pages/hotel/add_hotel_images_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddHotelImagesPage extends StatefulWidget {
+  const AddHotelImagesPage({super.key});
+
+  @override
+  State<AddHotelImagesPage> createState() => _AddHotelImagesPageState();
+}
+
+class _AddHotelImagesPageState extends State<AddHotelImagesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
